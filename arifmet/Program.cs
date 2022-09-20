bab5ad4566e09db717845a6ad0b1a50ff7c23a88
danil <danil@DESKTOP-KLFI3LI>
2022-09-20 02:12:48 +0200

    Добавьте файлы проекта.

---
+++ arifmet/Program.cs
@@ -0,0 +1,24 @@
+﻿//Голубихин Данила 22-ИСП-2/2 Вариант 20
+using System;
+
+namespace Arifmet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x: ");
+            string x = Console.ReadLine();
+            double x1=double.Parse(x);
+            Console.WriteLine("Введите y: ");
+            string y=Console.ReadLine();
+            double y1 = double.Parse(y);
+            double a = 1 + (Math.Sin(Math.Sqrt(x1 + 1)));
+            double b = Math.Cos(12 * y1 - 4);
+            double c = a / b;
+            int res = (int)Math.Round(c);
+            Console.WriteLine("Результат: ");
+            Console.WriteLine(res);
+        }
+    }
+}
